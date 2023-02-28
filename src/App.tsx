adde397345afc844e1770d9f1229f941b1c3eb47
Herley <herleyoliveira19@gmail.com>
2023-02-28 01:28:43 -0300

    create project of habits on react js

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+import './styles/global.css';
+
+import {
+    Plus,
+} from 'phosphor-react';
+
+// import {
+//     Habits,
+// } from './components/Habits';
+import logoImage from './assets/logo.svg';
+import {
+    Header,
+} from './components/Header';
+
+export function App() {
+  return (
+    <div className="w-screen h-screen flex justify-center items-center">
+      <div className="w-full max-w-5xl px-6 flex-col gap-16">
+        <Header />
+      </div>
+    </div>
+  );
+}
